91f1118854c32a4b19d73cb1a6a0620750c7d07f
YusufKhalifadev <yusufkhalifadev@gmail.com>
2024-08-30 14:22:09 +0300

    user_profile.ps1 refactoring

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,54 +1,41 @@
-# Ensure Terminal-Icons module is installed before importing
+# Module imports and initializations
 if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
-    Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck }
+    Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck
+}
 Import-Module -Name Terminal-Icons
 
-# Aliases & Functions
-Set-Alias -Name vim -Value nvim
-function rem($file) {
-    Remove-Item -Force -Recurse $file
-}
-function which($name) {
-    Get-Command $name | Select-Object -ExpandProperty Definition
-}
-function ll { Get-ChildItem -Path $pwd }
-#
-# Git Shortcuts
-function gs { git status }
-
-function gn { git init }
-
-function ga { git add . }
-
-function gc { param($m) git commit -m "$m" }
-
-function gp { git push }
-
-function g { __zoxide_z github }
-
-function gcl { git clone "$args" }
-
-function gcom {
-    git add .
-    git commit -m "$args"
-}
-function lazyg {
-    git add .
-    git commit -m "$args"
-    git push
-}
-
-Invoke-Expression (& { (zoxide init powershell --cmd cd --hook pwd | Out-String) })
+# Aliases
+Set-Alias vim nvim
+Set-Alias ll Get-ChildItem
 Set-Alias -Name z -Value __zoxide_z -Option AllScope -Scope Global -Force
 Set-Alias -Name zi -Value __zoxide_zi -Option AllScope -Scope Global -Force
 
-# the prompt
+# Functions
+function rem($file) { Remove-Item -Force -Recurse $file }
+function which($name) { Get-Command $name | Select-Object -ExpandProperty Definition }
+function cp($file) { Copy-Item -Force -Recurse $file }
+function profile { & $PROFILE }
 
-$env:POSH_GIT_ENABLED = $true
-$previousOutputEncoding = [Console]::OutputEncoding
-[Console]::OutputEncoding = [Text.Encoding]::UTF8
-try {
-    oh-my-posh.exe init pwsh --config $env:USERPROFILE\.config\ohmyposh\base.toml | Invoke-Expression
-} finally {
-    [Console]::OutputEncoding = $previousOutputEncoding
+# Git functions
+$gitFunctions = @{
+    gs   = { git status }
+    gn   = { git init }
+    gc   = { param($m) git commit -m $m }
+    gp   = { git push }
+    gcl  = { param($repo) git clone $repo }
+    lazyg = {
+        param($message)
+        git add .
+        git commit -m $message
+        git push
+    }
 }
+
+$gitFunctions.GetEnumerator() | ForEach-Object {
+    Set-Item -Path Function:$($_.Key) -Value $_.Value
+}
+
+# Initializations
+Invoke-Expression (& { (gh completion -s powershell | Out-String) })
+Invoke-Expression (& { (zoxide init powershell --cmd cd --hook pwd | Out-String) })
+Invoke-Expression (& { (oh-my-posh.exe init pwsh --config $env:USERPROFILE\.config\ohmyposh\base.toml | Out-String) })
